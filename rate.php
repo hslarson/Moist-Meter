2f00ba546a5ac2dde3d0ca7ad81fd7f6e54b0da5
Hunter Larson <hslarson@umail.ucsb.edu>
2021-08-05 00:46:36 -0700

    Cleaning Up

--- rate.php
+++ rate.php
@@ -3,8 +3,9 @@
     header('Content-Type: application/json');
 
     // Parse the Request
-    if (isset($_POST['id'], $_POST['score'])) {
+    if (isset($_POST['id'], $_POST['category'], $_POST['score'])) {
         $id = htmlentities($_POST['id']);
+        $category = htmlentities($_POST['category']);
         $score = htmlentities($_POST['score']);
     }
     
@@ -20,15 +21,15 @@
             $m = "Found Element, Adding Score";
             
             // Save Score
+            $data[$key]["category"] = $category;
             $data[$key]["score"] = $score;
             $data[$key]["rated"] = TRUE;
 
-            // Save the File
-            file_put_contents("../.data.json", json_encode($data, JSON_PRETTY_PRINT));
-            update();
+            file_put_contents("../.data.json", json_encode($data, JSON_PRETTY_PRINT)); // Save the File
+            update(); // Update the last_update Tag
             break;
         }
     }
-
+    // Return the JSON Response
     echo json_encode($m);
 ?>
--- update.php
+++ update.php
@@ -1,18 +1,22 @@
 <?php
     function update() {
+        // Load Main Webpage
         $site = file_get_contents("../index.html", FALSE);
 
+        // Find the Current Value of the last_update Tag
         $key_phrase = "last_update=";
         $start_i = strpos($site, $key_phrase);
         $end_i = strpos($site, "<", $start_i);
         if ($start_i == $end_i) { exit; }
-    
         $replace_target = substr($site, $start_i, $end_i - $start_i);
+
+        // Replace the Value of the last_update Tage
         $utc_time = strval(time());
         $replace_str = $key_phrase.$utc_time;
         $r_no = 1;
         $site = str_replace($replace_target, $replace_str, $site, $r_no);
         
+        // Save the Webpage
         file_put_contents("../index.html", $site);
     }
 ?>
