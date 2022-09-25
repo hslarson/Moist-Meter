75d343db9c7a53fc95ef77494c1e07f6d266b7ae
Hunter Larson <hslarson@umail.ucsb.edu>
2022-09-24 21:03:01 -0700

    Fix Indentation

--- Website/rate.php
+++ Website/rate.php
@@ -1,34 +1,34 @@
 <?php
-    header('Content-Type: application/json');
+	header('Content-Type: application/json');
 
-    // Parse the Request
-    if (isset($_POST['id'], $_POST['category'], $_POST['score'])) {
-        $id = htmlentities($_POST['id']);
-        $category = htmlentities($_POST['category']);
-        $score = htmlentities($_POST['score']);
-    }
-    
-    // Load the Data File
-    $data = json_decode(file_get_contents("../.data.json"), TRUE);
+	// Parse the Request
+	if (isset($_POST['id'], $_POST['category'], $_POST['score'])) {
+		$id = htmlentities($_POST['id']);
+		$category = htmlentities($_POST['category']);
+		$score = htmlentities($_POST['score']);
+	}
+	
+	// Load the Data File
+	$data = json_decode(file_get_contents("../.data.json"), TRUE);
 
-    // Find the Element We Want to Modify and Modify it
-    $m = "No Matching Element Found";
+	// Find the Element We Want to Modify and Modify it
+	$m = "No Matching Element Found";
 
-    foreach ($data as $key => $elem) {
-        
-        if ($elem['id'] == $id) {
-            $m = "Found Element, Adding Score";
-            
-            // Save Score
-            $data[$key]["category"] = $category;
-            $data[$key]["score"] = $score;
-            $data[$key]["rated"] = TRUE;
-            
-            // Save the File
-            file_put_contents("../.data.json", json_encode($data, JSON_PRETTY_PRINT));
-            break;
-        }
-    }
-    // Return the JSON Response
-    echo json_encode($m);
+	foreach ($data as $key => $elem) {
+		
+		if ($elem['id'] == $id) {
+			$m = "Found Element, Adding Score";
+			
+			// Save Score
+			$data[$key]["category"] = $category;
+			$data[$key]["score"] = $score;
+			$data[$key]["rated"] = TRUE;
+			
+			// Save the File
+			file_put_contents("../.data.json", json_encode($data, JSON_PRETTY_PRINT));
+			break;
+		}
+	}
+	// Return the JSON Response
+	echo json_encode($m);
 ?>
